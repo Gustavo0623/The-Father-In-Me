60ca450b952e2ec00f86a87d3e945da4b9d9ca20
Gustavo <gustavo_roger@yahoo.com>
2024-01-07 21:03:02 -0800

    image

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -3,7 +3,7 @@ import Image from "next/image"
 export default function Header () {
     return (
         <nav className="flex justify-between bg-slate-900 p-4 ">
-            <Image src="/logo.jpeg" alt="logo" width={300} height={100} className="w-auto h-auto rounded-xl" priority/>
+            <Image src="/logo.png" alt="logo" width={300} height={100} className="w-1/4 h-auto rounded-xl" priority/>
         </nav>
     )
 }
