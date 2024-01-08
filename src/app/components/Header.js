599a45e3a00816fc3789576da5d3e2981d700130
Gustavo <gustavo_roger@yahoo.com>
2024-01-07 21:22:53 -0800

    adjust image size

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -4,7 +4,7 @@ import logo from "../../../public/logo.png"
 export default function Header () {
     return (
         <nav className="flex justify-between bg-slate-900 p-4 ">
-            <Image src={logo} alt="logo" width={300} height={100} className="w-1/4 h-auto rounded-xl" priority/>
+            <Image src={logo} alt="logo" className="w-64 min-w-8 h-auto rounded-xl" priority/>
         </nav>
     )
 }
